8abbd9a3f9f0dc7e9509395259f91fe2566d43a2
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-20 02:06:10 +0800

    Update vscode中的jupyter配置R.sh

--- conda/vscode中的jupyter配置R.sh
+++ conda/vscode中的jupyter配置R.sh
@@ -7,12 +7,12 @@ conda install r-base=3.6.1 -y
 where R
 
 # 安装包
-conda install r-ggplot2
-conda install r-seurat
-conda install r-devtools
-conda install r-BiocManager
-conda install r-tidyverse
-conda install r-cowplot
+conda install r-ggplot2 -y
+conda install r-seurat -y
+conda install r-devtools -y
+conda install r-BiocManager -y
+conda install r-tidyverse -y
+conda install r-cowplot -y
 
 conda install -c conda-forge jupyterlab r-irkernel radian
 
