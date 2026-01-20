2a97774bf8d753a13bee0069bb789eba44f2e787
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-20 10:27:50 +0800

    Update and rename 安装及更新Miniconda3.sh to 配置Miniconda3.sh

---
+++ conda/配置Miniconda3.sh
@@ -0,0 +1,44 @@
+# 教程：https://docs.conda.io/projects/conda/en/stable/user-guide/install/windows.html
+# https://docs.conda.io/projects/conda/en/stable/index.html#
+###############################################################################################
+# 下载并且安装miniconda
+###############################################################################################
+cd /d D:\download
+curl -L -o Miniconda3-latest-Windows-x86_64.exe https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Windows-x86_64.exe
+setx PATH "D:\software\Miniconda3\condabin;D:\software\Miniconda3\Scripts;$env:PATH" # powershell
+
+#########################################################################################
+# 接受协议
+#########################################################################################
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/msys2
+
+#########################################################################################
+# 修改镜像
+#########################################################################################
+conda config --show channels
+conda config --remove-key channels
+conda config --add channels defaults
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/bioconda
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/
+conda config --set channel_priority strict
+conda config --show channels
+
+pip config list
+pip config unset global.index-url
+pip config unset global.extra-index-url
+
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip config set global.extra-index-url https://pypi.org/simple
+pip config list
+
+###############################################################################################
+# 更新conda
+###############################################################################################
+conda update conda
+
+###############################################################################################
+# 查看conda版本
+###############################################################################################
+conda --version
