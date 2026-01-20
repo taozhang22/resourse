ce430e92254abf0e673da01836e2b97192724718
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-20 10:09:34 +0800

    Update and rename 安装.sh to 安装及更新Miniconda3.sh

--- conda/安装及更新Miniconda3.sh
+++ conda/安装及更新Miniconda3.sh
@@ -4,8 +4,9 @@
 ###############################################################################################
 cd /d D:\download
 curl -L -o Miniconda3-latest-Windows-x86_64.exe https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Windows-x86_64.exe
-start /wait "" Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /D=D:\software\Miniconda3
+setx PATH "D:\software\Miniconda3\condabin;D:\software\Miniconda3\Scripts;$env:PATH" # powershell
 
 ###############################################################################################
 # 更新conda
 ###############################################################################################
+conda update conda
