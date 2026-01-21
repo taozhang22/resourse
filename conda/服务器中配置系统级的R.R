8613d28b00ea53019363c94e2a5e37cf7655dbb9
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-21 20:54:16 +0800

    Update 服务器中配置系统级的R.R

--- conda/服务器中配置系统级的R.R
+++ conda/服务器中配置系统级的R.R
@@ -3,5 +3,11 @@ wget https://cran.r-project.org/src/base/R-4/R-4.4.2.tar.gz
 tar -zxvf R-4.4.2.tar.gz
 cd R-4.4.2
 
+./configure \
+  --prefix=$HOME/software/R/4.4.2 \
+  --enable-R-shared \
+  --with-x=no
+make -j 4
+make install
 
 
