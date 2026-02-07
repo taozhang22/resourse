276a44142341527b795c291bd0188ca714ce018a
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-02-07 13:59:56 +0800

    Update 安装和卸载软件.sh

--- 乌班图/安装和卸载软件.sh
+++ 乌班图/安装和卸载软件.sh
@@ -2,3 +2,16 @@
 sudo apt purge -y r-base r-base-core r-recommended
 sudo apt autoremove --purge -y
 sudo apt clean
+
+# 卸载vscode
+sudo apt update
+sudo apt purge -y code code-insiders || true
+sudo apt autoremove --purge -y
+sudo apt clean
+
+# 卸载positron
+sudo apt update
+sudo apt purge -y positron
+sudo apt autoremove --purge -y
+sudo apt clean
+
